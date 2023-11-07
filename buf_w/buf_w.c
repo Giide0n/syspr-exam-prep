6a156f7a838605078c29272757b65f07b5586f8f
gideon <gideon.monterosa@gmail.com>
2023-11-07 12:52:31 +0100

    add buff_w

--- buf_w/buf_w.c
+++ buf_w/buf_w.c
@@ -3,14 +3,18 @@
 #include <printf.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    int fd = open(argv[1], O_RDWR, S_IRUSR | S_IWUSR);
-    char buff[1];
-    int r;
-
-    lseek(fd, 1, SEEK_SET);
-    read(fd, buff, 1);
-    int w = atoi(buff);
-
-    printf("%d\n", w);
+int main(int argc, char* argv[]){
+    char val = atoi(argv[2]);
+    int file = open(argv[1],O_RDWR);
+    lseek(file,1,SEEK_SET);
+    int pos;
+    read(file,&pos,1);
+    int bufsize = lseek(file,0,SEEK_END)-2;
+    lseek(file,pos+2,SEEK_SET);
+    write(file, &val, 1);
+    lseek(file,1,SEEK_SET);
+    pos = (pos+1)%bufsize;
+    write(file,&pos,1);
+    close(file);
+    return 0;
 }
