6c08565bb1ba31525ede08d510fc1b62671e08cd
benajakuhn <benaja.kuhn@students.fhnw.ch>
2024-01-06 14:50:34 +0100

    udp2tcp.c is totally NOT working yet

--- assessment2/udp2tcp/udp2tcp.c
+++ assessment2/udp2tcp/udp2tcp.c
@@ -6,5 +6,23 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
+int main(int argc, char *argv[]){
+    int buf[32]
 
+    int tcpsock = socket(AF_INET, SOCK_STREAM, 0);
+    int udpsock = socket(AF_INET, SOCK_DGRAM, 0);
+
+    struct sockaddr_in server_address;
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(8080);
+    server_address.sin_adress = "127.0.0.1";
+
+    recvfrom(udpsock, &buf, 4096, 0, &server_address, sizeof(server_address));
+
+    printf("Server Port: %d\n", ntohs(server_address.sin_port));
+}
