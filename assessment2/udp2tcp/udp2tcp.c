6c1a2e2b0cb125fd19b38819572ede4ca21166d9
benajakuhn <benaja.kuhn@students.fhnw.ch>
2024-01-06 16:40:41 +0100

    udp2tcp.c is totally working now

--- assessment2/udp2tcp/udp2tcp.c
+++ assessment2/udp2tcp/udp2tcp.c
@@ -3,26 +3,49 @@
 // Test: FS2022 4ibb2 Aufgabe 3
 //
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/socket.h>
+#include <string.h>
 #include <arpa/inet.h>
-#include <netinet/in.h>
+#include <sys/socket.h>
 
-int main(int argc, char *argv[]){
-    int buf[32]
+#define UDP_PORT 7070
+#define TCP_PORT 8080
+#define BUFFER_SIZE 4096
 
-    int tcpsock = socket(AF_INET, SOCK_STREAM, 0);
-    int udpsock = socket(AF_INET, SOCK_DGRAM, 0);
+struct sockaddr_in addr;
 
-    struct sockaddr_in server_address;
+void initAddr() {
+    const char *host = "0.0.0.0";
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(UDP_PORT);
+    inet_pton(AF_INET, host, &addr.sin_addr);
+}
 
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(8080);
-    server_address.sin_adress = "127.0.0.1";
+int main() {
+    int udp_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    int tcp_sock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in sender_addr;
+    char buffer[BUFFER_SIZE];
+    socklen_t sender_addr_size = sizeof(sender_addr);
 
-    recvfrom(udpsock, &buf, 4096, 0, &server_address, sizeof(server_address));
+    initAddr();
+    bind(udp_sock, (struct sockaddr *)&addr, sizeof(addr));
 
-    printf("Server Port: %d\n", ntohs(server_address.sin_port));
+    recvfrom(udp_sock, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&sender_addr, &sender_addr_size);
+
+    struct sockaddr_in tcp_server_addr = sender_addr;
+    tcp_server_addr.sin_port = htons(TCP_PORT);
+
+    connect(tcp_sock, (struct sockaddr *)&tcp_server_addr, sizeof(tcp_server_addr));
+    send(tcp_sock, buffer, BUFFER_SIZE, 0);
+
+    int received_bytes = recvfrom(tcp_sock, buffer, BUFFER_SIZE, 0);
+    write(STDOUT_FILENO, buffer, received_bytes);
+
+    close(tcp_sock);
+    close(udp_sock);
+
+    return 0;
 }
